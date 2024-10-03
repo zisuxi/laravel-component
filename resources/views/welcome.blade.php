6a8117517d7cc1db21f5faf7d4a51994466a63db
Talha Rehman <itsme.talha64@gmail.com>
2024-10-03 00:47:41 -0700

    laravel component

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+        <!-- =========================-- include Header Start ======================-->
+        <x-header-coponent />
+        <!-- =========================-- include Header Start ======================-->
+        <!-- =========================-- include Header Start ======================-->
+        <x-side-bar-component />
+
+        <!-- =========================-- include Header Start ======================-->
+        <div class="vertical-overlay"></div>
+        <div class="main-content">
+            <div class="page-content">
+                <div class="container-fluid">
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                <h4 class="mb-sm-0">Dashboard</h4>
+
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item">
+                                            <a href="{{ url('javascript: void(0);') }}">Dashboards</a>
+                                        </li>
+                                        <li class="breadcrumb-item active">Dashboard</li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+            <!-- =========================-- Footer Component-- =========================-->
+            <x-footer-component />
+
+            <!-- =========================-- Footer Component-- =========================-->
